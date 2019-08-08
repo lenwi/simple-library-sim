cdab7647aafbb6d89b838fdf569de444aae8e68e
Wen Li <i1s2b@ugrad.cs.ubc.ca>
2019-08-07 21:50:37 -0700

    11.1 pc (model 99%)

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -5,29 +5,46 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
 
-    Button button;
+    Stage window;
+    Scene scene1;
+    Scene scene2;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primarystage) {
-        primarystage.setTitle("LibraryFrontDesk");
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+        window.setTitle("LibraryFrontDesk");
 
-        button = new Button();
-        button.setText("click");
+        HBox top = new HBox();
+        Button b1 = new Button("file");
+        Button b2 = new Button("fil");
+        Button b3 = new Button("fi");
+        top.getChildren().addAll(b1, b2, b3);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        VBox left = new VBox();
+        Button b4 = new Button("s");
+        Button b5= new Button("fdl");
+        Button b6 = new Button("di");
+        top.getChildren().addAll(b4, b5, b6);
 
-        Scene scene = new Scene(layout, 300, 250);
-        primarystage.setScene(scene);
-        primarystage.show();
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(top);
+        borderPane.setLeft(left);
+
+        Scene scene = new Scene(borderPane, 700, 250);
+        window.setScene(scene);
+        window.show();
     }
 }
